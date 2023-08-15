3d99f2c57e2be941ab3dbf6226bc841712963e65
Nimish kumar <nimishkumar9534@gmail.com>
2023-08-15 14:03:36 +0530

    Join Us Data added

---
+++ src/components/JoinUsSection/JoinUsData.js
@@ -0,0 +1,31 @@
+const JoinUsData = [
+  {
+    Heading: "NOTEWORTHY DIVERSE TEAM",
+    SubHeading: "Get a better engagement with the diverse team of doctors,designers, developers and many others.",
+    NumberIcon: "https://amciedtech.com/assets/newimage/w1.png",
+    MainImage: "https://amciedtech.com/assets/newimage/w12.png"
+  },
+  {
+    Heading: "DIVERTING WORK CULTURE",
+    SubHeading: "Get along with a work culture that focuses on innovation, deep work, and improvement.",
+    NumberIcon: "https://amciedtech.com/assets/newimage/w2.png",
+    MainImage: "https://amciedtech.com/assets/newimage/w22.png"
+  },
+  {
+    Heading: "SELF IMPROVEMENT",
+    SubHeading: "We guide how to invest yourself to strengthen your foundation for greater achievements.",
+    NumberIcon: "https://amciedtech.com/assets/newimage/w3.png",
+    MainImage: "https://amciedtech.com/assets/newimage/w33.png"
+  },
+  {
+    Heading: "FEARLESS PRODUCTS",
+    SubHeading: "We assure to deliver advanced tools for all medicos and others for a hassle-free utilization.",
+    NumberIcon: "https://amciedtech.com/assets/newimage/w4.png",
+    MainImage: "https://amciedtech.com/assets/newimage/w44.png"
+  },
+];
+
+export default JoinUsData;
+
+// Middle Image link
+// https://amciedtech.com/assets/newimage/wm.png
