3181615b4e5bdb24fcf1d670bb3cd0bdeda14a5f
Ankush8950 <69650280+Ankush8950@users.noreply.github.com>
2023-08-24 00:25:07 +0530

    whatsapp api done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import {BsWhatsapp} from "react-icons/bs"
 
 const Header = () => {
   return (
+    <div className='flex'>
     <div className='w-full h-full pt-2' style={{backgroundImage: 'url(https://www.mindteck.com/assets/sliders/63366092_medical-devices-dts.jpg)',width: '100%',height: '100%'}}>
         {/* <img src="" alt="" /> */}
         <div className='flex flex-col items-center justify-center w-full h-screen'>
             <h1 className='font-bold text-4xl text-white'>CAREERS</h1>
             <p className='font-semibold text-lg pl-20 pr-20 pt-10 text-white text-center'>At AMCI, we are looking for someone who is having a mind full of creativity and willing to expand their helping hands to make the platform captivating for the country’s medical field. If you decide to join us, you will be getting an awe-inspiring chance to work with a diverse team of doctors from all over the nation, hard-working engineers, talented designers, and many other folks to bring a brighter future for this platform.</p>
         </div>
+    </div>
+    <div className='fixed bottom-10 right-10 z-20 bg-[#44C054] p-2 rounded'>
+      <a href="https://wa.me/7282088791">
+        <BsWhatsapp size={40} color='white'/>
+      </a>
+    </div>
     </div>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,67 +1,78 @@
 import React, { useState } from "react";
-import Logo from "../Image/logo.jpg"
-import {RiMenuFill} from "react-icons/ri"
-import ToggleMenu from "../ToggleMenu/ToggleMenu"
-import {AiOutlineClose} from "react-icons/ai"
+import Logo from "../Image/logo.jpg";
+import { RiMenuFill } from "react-icons/ri";
+import ToggleMenu from "../ToggleMenu/ToggleMenu";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
-  const [ToggleMenus,setToggleMenu] = useState(false)
+  const [ToggleMenus, setToggleMenu] = useState(false);
   return (
-      <div className="navbar bg-white pl-10 pr-10 font-roboto w-full flex items-center justify-between">
-        <div className="">
-          <img  src={Logo} className="rounded w-[200px]" alt="" />
-        </div>
-        <div className="hidden lg:flex">
-          <ul className="menu menu-horizontal px-1 w-full">
-            <li>
-              <a className="text-[#04789D] hover:text-[#04789D]">About Us</a>
-            </li>
-            <li tabIndex={0} className="">
-              <details>
-                <summary className="text-[#04789D] hover:text-[#04789D]">Industries</summary>
-                <ul className="p-2 w-[200px]">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li tabIndex={0} className="">
-              <details>
-                <summary className="text-[#04789D] hover:text-[#04789D]">Technology</summary>
-                <ul className="p-2 w-[200px]">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a className="text-[#04789D] hover:text-[#04789D]">Services</a>
-            </li>
-            <li>
-              <a className="text-[#04789D] hover:text-[#04789D]">Careers</a>
-            </li>
-            <li>
-              <a className="text-[#04789D] hover:text-[#04789D]">Contact Us</a>
-            </li>
-          </ul>
-        </div>
-       <div className="flex  lg:hidden" onClick={()=>setToggleMenu(!ToggleMenus)}>
-        {
-          ToggleMenus ? <AiOutlineClose size={40}/> : <RiMenuFill size={40}/>
-        }
-       </div>
-       {
-        ToggleMenus ? <ToggleMenu /> : ""
-       }
+    <div className="navbar bg-white pl-10 pr-10 font-roboto w-full flex items-center justify-between">
+      <div className="">
+        <img src={Logo} className="rounded w-[200px]" alt="" />
       </div>
+      <div className="hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 w-full">
+          <li>
+            <a className="text-[#04789D] hover:text-[#04789D]">About Us</a>
+          </li>
+            <li className="dropdown dropdown-hover">
+              <label
+                tabIndex={0}
+                className="text-[#04789D] hover:text-[#04789D]"
+              >
+                Industries
+              </label>
+              <ul
+                tabIndex={0}
+                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a>Item 1</a>
+                </li>
+                <li>
+                  <a>Item 2</a>
+                </li>
+              </ul>
+            </li>
+             <li className="dropdown dropdown-hover">
+              <label
+                tabIndex={0}
+                 className = "text-[#04789D] hover:text-[#04789D]" >
+                   Technology
+              </label>
+              <ul
+                tabIndex={0}
+                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a>Internet of Things</a>
+                </li>
+                <li>
+                  <a>Item 2</a>
+                </li>
+              </ul>
+            </li>
+         
+          <li>
+            <a className="text-[#04789D] hover:text-[#04789D]">Services</a>
+          </li>
+          <li>
+            <a className="text-[#04789D] hover:text-[#04789D]">Careers</a>
+          </li>
+          <li>
+            <a className="text-[#04789D] hover:text-[#04789D]">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+      <div
+        className="flex  lg:hidden"
+        onClick={() => setToggleMenu(!ToggleMenus)}
+      >
+        {ToggleMenus ? <AiOutlineClose size={40} /> : <RiMenuFill size={40} />}
+      </div>
+      {ToggleMenus ? <ToggleMenu /> : ""}
+    </div>
   );
 };
 
